e7712a994ef8b9dbba2576d12779d26de1d0aad5
jiyang <jiyang001@huazhu.com>
2015-03-24 17:37:21 +0800

    add

--- src/wa_common_track.js
+++ src/wa_common_track.js
@@ -7,9 +7,18 @@
 // 事件提交 （事件类别，事件动作，事件标签，事件值）
 //
 function wa_track_event(cate, action, label, value) {
-	
+	cate = cate || "";
+	action = action || "";
+	label = label || "";
+	value = value || 0;
+
+	_gaq.push([cate, action, label]);
+	_paq.push(cate, action, label, value);
 }
 
+//
+// 自定义变量
+//
 function wa_set_var(key, value) {
-	
+
 }
